95c6df34d2257b48b56c850c1d8ccaa9c01c6956
Peter Feerick <peter.feerick@gmail.com>
2019-01-03 16:01:27 +1000

    Code autoformat

--- src/main.cpp
+++ src/main.cpp
@@ -44,19 +44,19 @@ A - Accelerometer
 #define MPU_addr 0x68
 
 #include <Arduino.h>
-#include <ESP8266WiFi.h>          //ESP8266 Core WiFi Library (you most likely already have this in your sketch)
+#include <ESP8266WiFi.h> //ESP8266 Core WiFi Library (you most likely already have this in your sketch)
 
-#include <DNSServer.h>            //Local DNS Server used for redirecting all requests to the configuration portal
-#include <ESP8266WebServer.h>     //Local WebServer used to serve the configuration portal
-#include <WiFiManager.h>          //https://github.com/tzapu/WiFiManager WiFi Configuration Magic
+#include <DNSServer.h>        //Local DNS Server used for redirecting all requests to the configuration portal
+#include <ESP8266WebServer.h> //Local WebServer used to serve the configuration portal
+#include <WiFiManager.h>      //https://github.com/tzapu/WiFiManager WiFi Configuration Magic
 
 #include <ESP8266mDNS.h>
-#include <WiFiUdp.h>              // For NTP
+#include <WiFiUdp.h> // For NTP
 
-#include <ArduinoOTA.h>           // For OTA programming
-#include <TimeLib.h>              // Friendly Time formatting and timekeeping
+#include <ArduinoOTA.h> // For OTA programming
+#include <TimeLib.h>    // Friendly Time formatting and timekeeping
 
-#include <SPI.h>                  // For LED Panel
+#include <SPI.h> // For LED Panel
 #include <Adafruit_GFX.h>
 #include <Max72xxPanel.h>
 
@@ -70,37 +70,37 @@ Button button(0); // Connect your button between P2/GPIO0 and GND
 #define DEBUG_OI Serial
 
 #ifdef DEBUG
-#define   DebugBegin(...) DEBUG_OI.begin(__VA_ARGS__)
-#define   DebugPrint(...) DEBUG_OI.print(__VA_ARGS__)
-#define   DebugPrintln(...) DEBUG_OI.println(__VA_ARGS__)
-#define   DebugPrintf(...) DEBUG_OI.printf(__VA_ARGS__)
-#define   DebugWrite(...) DEBUG_OI.write(__VA_ARGS__)
+#define DebugBegin(...) DEBUG_OI.begin(__VA_ARGS__)
+#define DebugPrint(...) DEBUG_OI.print(__VA_ARGS__)
+#define DebugPrintln(...) DEBUG_OI.println(__VA_ARGS__)
+#define DebugPrintf(...) DEBUG_OI.printf(__VA_ARGS__)
+#define DebugWrite(...) DEBUG_OI.write(__VA_ARGS__)
 #else
 // Define the counterparts that cause the compiler to generate no code
-#define   DebugBegin(...) (void(0))
-#define   DebugPrint(...) (void(0))
-#define   DebugPrintln(...) (void(0))
-#define   DebugPrintf(...) (void(0))
-#define   DebugWrite(...) (void(0))
+#define DebugBegin(...) (void(0))
+#define DebugPrint(...) (void(0))
+#define DebugPrintln(...) (void(0))
+#define DebugPrintf(...) (void(0))
+#define DebugWrite(...) (void(0))
 #endif
 
 /************************* NTP SETUP ****************************************/
 static const char ntpServerName[] = "au.pool.ntp.org";
-const int timeZone = 10;     // AEST
+const int timeZone = 10; // AEST
 
 // A UDP instance to let us send and receive packets over UDP
 WiFiUDP udp;
-unsigned int localPort = 2390;      // local port to listen for UDP packets
+unsigned int localPort = 2390; // local port to listen for UDP packets
 
 /************************* LED MATRIX SETUP *********************************/
-int pinCS = 16; // Attach CS to this pin, DIN to MOSI and CLK to SCK (cf http://arduino.cc/en/Reference/SPI )
-int numberOfHorizontalDisplays = 4;//adjust this to your setup
+int pinCS = 16;                     // Attach CS to this pin, DIN to MOSI and CLK to SCK (cf http://arduino.cc/en/Reference/SPI )
+int numberOfHorizontalDisplays = 4; //adjust this to your setup
 int numberOfVerticalDisplays = 1;
 
 Max72xxPanel matrix = Max72xxPanel(pinCS, numberOfHorizontalDisplays, numberOfVerticalDisplays);
 
 bool useIMU = true;
-int16_t AcX,AcY,AcZ,Tmp,GyX,GyY,GyZ;
+int16_t AcX, AcY, AcZ, Tmp, GyX, GyY, GyZ;
 
 /************************* FUNCTION PROTOTYPES ******************************/
 time_t getNtpTime();
@@ -109,8 +109,9 @@ void printDigits(int digits);
 void sendNTPpacket(IPAddress &address);
 byte check_I2c(byte addr);
 
-void configModeCallback (WiFiManager *myWiFiManager) {
-  matrix.fillScreen(LOW);//Empty the screen
+void configModeCallback(WiFiManager *myWiFiManager)
+{
+  matrix.fillScreen(LOW); //Empty the screen
   matrix.setCursor(0, 0); //Move the cursor to the end of the screen
   matrix.print("WM CFG");
   matrix.write();
@@ -120,23 +121,30 @@ void configModeCallback (WiFiManager *myWiFiManager) {
   DebugPrintln(myWiFiManager->getConfigPortalSSID());
 }
 
-void setup() {
+void setup()
+{
   DebugBegin(115200);
-  
+
   DebugPrintln("");
   DebugPrintln(F("ESP8266 NTP Clock loading..."));
-  DebugPrint("Compiled: " __DATE__ ", " __TIME__ ", Arduino IDE v"); DebugPrintln(ARDUINO,DEC); 
-  DebugPrint(F("Core version: ")); DebugPrintln(ESP.getCoreVersion());
-  DebugPrint(F("SDK version: ")); DebugPrintln(ESP.getSdkVersion());
-  DebugPrint(F("Chip ID: ")); DebugPrintln(ESP.getChipId());
-  DebugPrint(F("Reset reason: ")); DebugPrintln(ESP.getResetReason());
-  DebugPrint(F("Heap free: ")); DebugPrintln(ESP.getFreeHeap());
+  DebugPrint("Compiled: " __DATE__ ", " __TIME__ ", Arduino IDE v");
+  DebugPrintln(ARDUINO, DEC);
+  DebugPrint(F("Core version: "));
+  DebugPrintln(ESP.getCoreVersion());
+  DebugPrint(F("SDK version: "));
+  DebugPrintln(ESP.getSdkVersion());
+  DebugPrint(F("Chip ID: "));
+  DebugPrintln(ESP.getChipId());
+  DebugPrint(F("Reset reason: "));
+  DebugPrintln(ESP.getResetReason());
+  DebugPrint(F("Heap free: "));
+  DebugPrintln(ESP.getFreeHeap());
   DebugPrintln("");
-  
+
   //0 low, 15 high
   matrix.setIntensity(1); // Set brightness between 0 and 15
 
-  matrix.setRotation(0, 1);//you may have to change this section depending on your LED matrix setup
+  matrix.setRotation(0, 1); //you may have to change this section depending on your LED matrix setup
   matrix.setRotation(1, 1);
   matrix.setRotation(2, 1);
   matrix.setRotation(3, 1);
@@ -154,21 +162,22 @@ void setup() {
 #endif
 
   // Initalise GY-521 / MPU-6050
-  Wire.begin(P0,P5); //SDA, SCL
-  
+  Wire.begin(P0, P5); //SDA, SCL
+
   check_I2c(MPU_addr);
 
   Wire.beginTransmission(MPU_addr);
-  Wire.write(0x6B);  // PWR_MGMT_1 register
-  Wire.write(0);     // set to zero (wakes up the MPU-6050)
+  Wire.write(0x6B); // PWR_MGMT_1 register
+  Wire.write(0);    // set to zero (wakes up the MPU-6050)
   Wire.endTransmission(true);
 
-  matrix.fillScreen(LOW);//Empty the screen
+  matrix.fillScreen(LOW); //Empty the screen
   matrix.setCursor(0, 0); //Move the cursor to the end of the screen
   matrix.print("WiFi");
   matrix.write();
 
-  if (!wifiManager.autoConnect()) {
+  if (!wifiManager.autoConnect())
+  {
     DebugPrintln("Failed to connect and hit timeout");
     //reset and try again, or maybe put it to deep sleep
     ESP.reset();
@@ -181,7 +190,7 @@ void setup() {
 
   ArduinoOTA.onStart([]() {
     DebugPrintln("OTA Programming Start");
-    matrix.fillScreen(LOW);//Empty the screen
+    matrix.fillScreen(LOW); //Empty the screen
     matrix.setCursor(0, 0); //Move the cursor to the end of the screen
     matrix.print("OTA");
     matrix.write();
@@ -189,7 +198,7 @@ void setup() {
 
   ArduinoOTA.onEnd([]() {
     DebugPrintln("\nOTA Programming End");
-    matrix.fillScreen(LOW);//Empty the screen
+    matrix.fillScreen(LOW); //Empty the screen
     matrix.setCursor(0, 0); //Move the cursor to the end of the screen
     matrix.print("DONE!");
     matrix.write();
@@ -198,7 +207,7 @@ void setup() {
   ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
     DebugPrintf("Progress: %u%%\r", (progress / (total / 100)));
 
-    matrix.fillScreen(LOW);//Empty the screen
+    matrix.fillScreen(LOW); //Empty the screen
     matrix.setCursor(0, 0); //Move the cursor to the end of the screen
     matrix.print("P");
 
@@ -207,7 +216,8 @@ void setup() {
     matrix.drawPixel(7, 4, HIGH);
 
     matrix.setCursor(9, 0);
-    if ((progress / (total / 100)) < 10) {
+    if ((progress / (total / 100)) < 10)
+    {
       matrix.print("0");
     }
     matrix.print((progress / (total / 100)));
@@ -217,13 +227,19 @@ void setup() {
 
   ArduinoOTA.onError([](ota_error_t error) {
     DebugPrintf("Error[%u]: ", error);
-    if (error == OTA_AUTH_ERROR) DebugPrintln("Auth Failed");
-    else if (error == OTA_BEGIN_ERROR) DebugPrintln("Begin Failed");
-    else if (error == OTA_CONNECT_ERROR) DebugPrintln("Connect Failed");
-    else if (error == OTA_RECEIVE_ERROR) DebugPrintln("Receive Failed");
-    else if (error == OTA_END_ERROR) DebugPrintln("End Failed");
+    
+    if (error == OTA_AUTH_ERROR)
+      DebugPrintln("Auth Failed");
+    else if (error == OTA_BEGIN_ERROR)
+      DebugPrintln("Begin Failed");
+    else if (error == OTA_CONNECT_ERROR)
+      DebugPrintln("Connect Failed");
+    else if (error == OTA_RECEIVE_ERROR)
+      DebugPrintln("Receive Failed");
+    else if (error == OTA_END_ERROR)
+      DebugPrintln("End Failed");
 
-    matrix.fillScreen(LOW);//Empty the screen
+    matrix.fillScreen(LOW); //Empty the screen
     matrix.setTextSize(1);
     matrix.setCursor(0, 0); //Move the cursor to the end of the screen
     matrix.print("OTA ER");
@@ -239,7 +255,7 @@ void setup() {
   DebugPrint("*UDP: Running on local port ");
   DebugPrintln(udp.localPort());
 
-  matrix.fillScreen(LOW);//Empty the screen
+  matrix.fillScreen(LOW); //Empty the screen
   matrix.setCursor(0, 0); //Move the cursor to the end of the screen
   matrix.print("Ready");
   matrix.write();
@@ -252,36 +268,43 @@ void setup() {
 
 time_t prevDisplay = 0; // when the digital clock was displayed
 
-void loop() {
+void loop()
+{
   ArduinoOTA.handle();
 
-  if (timeStatus() != timeNotSet) {
-    if (now() != prevDisplay) { //update the display only if time has changed
+  if (timeStatus() != timeNotSet)
+  {
+    if (now() != prevDisplay)
+    { //update the display only if time has changed
       prevDisplay = now();
       digitalClockDisplay();
 
-      if (useIMU) {
+      if (useIMU)
+      {
         //query IMU
         Wire.beginTransmission(MPU_addr);
-        Wire.write(0x3B);  // starting with register 0x3B (ACCEL_XOUT_H)
+        Wire.write(0x3B); // starting with register 0x3B (ACCEL_XOUT_H)
         Wire.endTransmission(false);
-        Wire.requestFrom(MPU_addr,6,true);  // request a total of 14 registers
-        AcX=Wire.read()<<8|Wire.read();  // 0x3B (ACCEL_XOUT_H) & 0x3C (ACCEL_XOUT_L)    
-        AcY=Wire.read()<<8|Wire.read();  // 0x3D (ACCEL_YOUT_H) & 0x3E (ACCEL_YOUT_L)
-        AcZ=Wire.read()<<8|Wire.read();  // 0x3F (ACCEL_ZOUT_H) & 0x40 (ACCEL_ZOUT_L)
+        Wire.requestFrom(MPU_addr, 6, true);  // request a total of 14 registers
+        AcX = Wire.read() << 8 | Wire.read(); // 0x3B (ACCEL_XOUT_H) & 0x3C (ACCEL_XOUT_L)
+        AcY = Wire.read() << 8 | Wire.read(); // 0x3D (ACCEL_YOUT_H) & 0x3E (ACCEL_YOUT_L)
+        AcZ = Wire.read() << 8 | Wire.read(); // 0x3F (ACCEL_ZOUT_H) & 0x40 (ACCEL_ZOUT_L)
         // Tmp=Wire.read()<<8|Wire.read();  // 0x41 (TEMP_OUT_H) & 0x42 (TEMP_OUT_L)
         // GyX=Wire.read()<<8|Wire.read();  // 0x43 (GYRO_XOUT_H) & 0x44 (GYRO_XOUT_L)
         // GyY=Wire.read()<<8|Wire.read();  // 0x45 (GYRO_YOUT_H) & 0x46 (GYRO_YOUT_L)
         // GyZ=Wire.read()<<8|Wire.read();  // 0x47 (GYRO_ZOUT_H) & 0x48 (GYRO_ZOUT_L)
 
         //output
-        DebugPrint("AcX = "); DebugPrint(AcX);
-        DebugPrint(" | AcY = "); DebugPrint(AcY);
-        DebugPrint(" | AcZ = "); DebugPrintln(AcZ);
-        // DebugPrint(" | Tmp = "); DebugPrint(Tmp);  
+        DebugPrint("AcX = ");
+        DebugPrint(AcX);
+        DebugPrint(" | AcY = ");
+        DebugPrint(AcY);
+        DebugPrint(" | AcZ = ");
+        DebugPrintln(AcZ);
+        // DebugPrint(" | Tmp = "); DebugPrint(Tmp);
         // DebugPrint(" | GyX = "); DebugPrint(GyX);
         // DebugPrint(" | GyY = "); DebugPrint(GyY);
-        // DebugPrint(" | GyZ = "); DebugPrintln(GyZ);      
+        // DebugPrint(" | GyZ = "); DebugPrintln(GyZ);
       }
     }
   }
@@ -294,7 +317,8 @@ void loop() {
     unsigned int spacer = 1;
     unsigned int width = 5 + spacer; // The font width is 5 pixels
 
-    for ( unsigned int i = 0 ; i < width * tape.length() + matrix.width() - 1 - spacer; i++ ) {
+    for (unsigned int i = 0; i < width * tape.length() + matrix.width() - 1 - spacer; i++)
+    {
 
       matrix.fillScreen(LOW);
 
@@ -302,8 +326,10 @@ void loop() {
       int x = (matrix.width() - 1) - i % width;
       int y = (matrix.height() - 8) / 2; // center the text vertically
 
-      while ( x + width - spacer >= 0 && letter >= 0 ) {
-        if ( letter < tape.length() ) {
+      while (x + width - spacer >= 0 && letter >= 0)
+      {
+        if (letter < tape.length())
+        {
           matrix.drawChar(x, y, tape[letter], HIGH, LOW, 1);
         }
 
@@ -322,12 +348,12 @@ void loop() {
 
 void digitalClockDisplay()
 {
-  matrix.fillScreen(LOW);//Empty the screen
+  matrix.fillScreen(LOW); //Empty the screen
   matrix.setCursor(0, 0); //Move the cursor to the end of the screen
   if (hourFormat12() < 10)
     matrix.print(" ");
 
-  matrix.print(hourFormat12());//Write the time
+  matrix.print(hourFormat12()); //Write the time
 
   if ((second() % 2) != 0)
   {
@@ -342,7 +368,8 @@ void digitalClockDisplay()
   }
 
   matrix.setCursor(14, 0); //Move the cursor to the end of the screen
-  if (minute() < 10) {
+  if (minute() < 10)
+  {
     matrix.print("0");
   }
   matrix.print(minute());
@@ -369,7 +396,6 @@ void digitalClockDisplay()
     matrix.drawPixel(31, 4, HIGH); // *
     matrix.drawPixel(31, 5, HIGH); // *
     matrix.drawPixel(31, 6, HIGH); // *
-
   }
   else
   {
@@ -417,14 +443,15 @@ void printDigits(int digits)
 
 /*-------- NTP code ----------*/
 
-const int NTP_PACKET_SIZE = 48; // NTP time is in the first 48 bytes of message
+const int NTP_PACKET_SIZE = 48;     // NTP time is in the first 48 bytes of message
 byte packetBuffer[NTP_PACKET_SIZE]; //buffer to hold incoming & outgoing packets
 
 time_t getNtpTime()
 {
   IPAddress ntpServerIP; // NTP server's ip address
 
-  while (udp.parsePacket() > 0) ; // discard any previously received packets
+  while (udp.parsePacket() > 0)
+    ; // discard any previously received packets
   DebugPrintln("Transmit NTP Request");
   // get a random server from the pool
   WiFi.hostByName(ntpServerName, ntpServerIP);
@@ -433,14 +460,16 @@ time_t getNtpTime()
   DebugPrintln(ntpServerIP);
   sendNTPpacket(ntpServerIP);
   uint32_t beginWait = millis();
-  while (millis() - beginWait < 1500) {
+  while (millis() - beginWait < 1500)
+  {
     int size = udp.parsePacket();
-    if (size >= NTP_PACKET_SIZE) {
+    if (size >= NTP_PACKET_SIZE)
+    {
       DebugPrintln("Receive NTP Response");
-      udp.read(packetBuffer, NTP_PACKET_SIZE);  // read packet into the buffer
+      udp.read(packetBuffer, NTP_PACKET_SIZE); // read packet into the buffer
       unsigned long secsSince1900;
       // convert four bytes starting at location 40 to a long integer
-      secsSince1900 =  (unsigned long)packetBuffer[40] << 24;
+      secsSince1900 = (unsigned long)packetBuffer[40] << 24;
       secsSince1900 |= (unsigned long)packetBuffer[41] << 16;
       secsSince1900 |= (unsigned long)packetBuffer[42] << 8;
       secsSince1900 |= (unsigned long)packetBuffer[43];
@@ -458,10 +487,10 @@ void sendNTPpacket(IPAddress &address)
   // set all bytes in the buffer to 0
   memset(packetBuffer, 0, NTP_PACKET_SIZE);
   // Initialize values needed to form NTP request
-  packetBuffer[0] = 0b11100011;   // LI, Version, Mode
-  packetBuffer[1] = 0;     // Stratum, or type of clock
-  packetBuffer[2] = 6;     // Polling Interval
-  packetBuffer[3] = 0xEC;  // Peer Clock Precision
+  packetBuffer[0] = 0b11100011; // LI, Version, Mode
+  packetBuffer[1] = 0;          // Stratum, or type of clock
+  packetBuffer[2] = 6;          // Polling Interval
+  packetBuffer[3] = 0xEC;       // Peer Clock Precision
   // 8 bytes of zero for Root Delay & Root Dispersion
   packetBuffer[12] = 49;
   packetBuffer[13] = 0x4E;
@@ -476,7 +505,7 @@ void sendNTPpacket(IPAddress &address)
 
 byte check_I2c(byte addr)
 {
-   // We are using the return value of
+  // We are using the return value of
   // the Write.endTransmisstion to see if
   // a device did acknowledge to the address.
   byte error;
@@ -484,16 +513,16 @@ byte check_I2c(byte addr)
   error = Wire.endTransmission();
   DebugPrintln(" ");
   if (error == 0)
-    {
-      DebugPrint(" Device Found at 0x");
-      DebugPrintln(addr,HEX);
-      useIMU = true; 
-    }
+  {
+    DebugPrint(" Device Found at 0x");
+    DebugPrintln(addr, HEX);
+    useIMU = true;
+  }
   else
-    {
-      DebugPrint(" No Device Found at 0x");
-      DebugPrintln(addr,HEX);
-      useIMU = false;
-    }
+  {
+    DebugPrint(" No Device Found at 0x");
+    DebugPrintln(addr, HEX);
+    useIMU = false;
+  }
   return error;
 }
